e8bc463d33c69e87a03dcda4653a54b3a08f026f
gautam-dev-maker <64469823+gautam-dev-maker@users.noreply.github.com>
2020-09-04 14:55:09 +0530

    Kernel operation added

--- 2.Applying_Kernels/blurring.py
+++ 2.Applying_Kernels/blurring.py
@@ -3,7 +3,6 @@ import numpy as np
 
 def convolve3d(image, kernel):
     threshold=200
-    #kernel = np.flipud(np.fliplr(kernel))
     output = np.zeros_like(image)
     image_padded = np.zeros((image.shape[0]+4,image.shape[1] + 4,image.shape[2]))
     image_padded[3:-1:,3:-1:,:] = image
@@ -19,21 +18,14 @@ gaussian_blurr=np.array([[1,  4,  6,  4, 1],
                          [4, 16, 24, 16, 4],
                          [6, 24, 36, 24, 6],
                          [4, 16, 24, 16, 4],
-                         [1,  4,  6,  4, 1]])
+                         [1,  4,  6,  4, 1]])/256
 
 box_blur=np.array([[1,1,1],
                    [1,1,1],
                    [1,1,1]])
-# Sharpen=np.array([[0 ,-1, 0],
-#                   [-1, 5,-1],
-#                   [0 ,-1, 0]])
-# Edge=np.array([[-1 ,-1, -1],
-#                   [-1, 8,-1],
-#                   [-1 ,-1, -1]])
-#file_name=input("Enter the name of the image to blurr:- ")
 file_name="test_blur.jpg"
 im = np.array(Image.open(file_name))
-pil_img=Image.fromarray(convolve3d(im, gaussian_blurr/256))
+pil_img=Image.fromarray(convolve3d(im, gaussian_blurr))
 pil_img.save('result_blur.jpg')
 
 
--- 2.Applying_Kernels/sharpen.py
+++ 2.Applying_Kernels/sharpen.py
@@ -1,35 +1,30 @@
 from PIL import Image
 import numpy as np
 def convolve3d(image, kernel):
-    threshold=100
-    kernel = np.flipud(np.fliplr(kernel))
     output = np.zeros_like(image)
-    image_padded = np.zeros((image.shape[0]+2,image.shape[1] + 2,image.shape[2]))
-    image_padded[1:-1:,1:-1:,:] = image
+    image_padded = np.zeros((image.shape[0]+kernel.shape[0]-1,image.shape[1] + kernel.shape[1]-1,image.shape[2]))
+    image_padded[kernel.shape[0]-2:-1:,kernel.shape[1]-2:-1:,:] = image
     image_padded[0,0,:]=image[0,0,:]
     image_padded[-1,-1,:]=image[-1,-1,:]
     for x in range(image.shape[1]):
         for y in range(image.shape[0]):
             for z in range(image.shape[2]):
-                summation=(kernel * image_padded[y: y+3, x: x+3,z]).sum()
-                if summation<threshold :
-                    output[y, x,z]=50
-                else:
-                    output[y, x,z]=summation
+                summation=(kernel * image_padded[y: y+kernel.shape[0], x: x+kernel.shape[1],z]).sum()
+                output[y, x,z]=summation
     output[:,:,3]=image[:,:,3]
     return output
 
+Sharpen1=np.array([[-1, -1, -1, -1, -1],
+                   [-1,  2,  2,  2, -1],
+                   [-1,  2,  8,  2, -1],
+                   [-1,  2,  2,  2, -1],
+                   [-1, -1, -1, -1, -1,]])
 
-Sharpen=np.array([[-1,-1, -1],
-                  [-1, 9,-1],
-                  [-1 ,-1, -1]])
-# Edge=np.array([   [0 ,-1, 0],
-#                   [-1, 4,-1],
-#                   [0 ,-1, 0]])
-                  
+Sharpen=np.array([[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1],[-1,-1,25,-1,-1],[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1]])/-25
 file_name="test_sharpen.png"
 im = np.array(Image.open(file_name))
-pil_img=Image.fromarray(convolve3d(im,Sharpen*0.9))
+im=convolve3d(im,Sharpen)
+pil_img=Image.fromarray(im.astype(np.uint8))
 pil_img.save('result_sharpen.png')
 
 
